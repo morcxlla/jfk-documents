8e4476dc152a7a2319777c4a66d5ac0a73429fb6
morcxlla <jorgecividanes2016@gmail.com>
2025-03-21 21:35:40 +0100

    Initial Commit (Source: https://vercel.com/templates/next.js/nextjs-portfolio)

---
+++ app/components/footer.tsx
@@ -0,0 +1,69 @@
+import Link from "next/link";
+
+function ArrowIcon() {
+  return (
+    <svg
+      width="12"
+      height="12"
+      viewBox="0 0 12 12"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M2.07102 11.3494L0.963068 10.2415L9.2017 1.98864H2.83807L2.85227 0.454545H11.8438V9.46023H10.2955L10.3097 3.09659L2.07102 11.3494Z"
+        fill="currentColor"
+      />
+    </svg>
+  );
+}
+
+export default function Footer() {
+  return (
+    <footer className="mb-16">
+      <ul className="flex md:flex-row flex-col space-x-0 md:space-x-4 space-y-2 md:space-y-0 mt-8 font-sm text-neutral-600 dark:text-neutral-300">
+        <li>
+          <Link
+            className="flex items-center hover:text-neutral-800 dark:hover:text-neutral-100 transition-all"
+            rel="noopener noreferrer"
+            target="_blank"
+            href="/rss"
+          >
+            <ArrowIcon />
+            <p className="ml-2 h-7">rss</p>
+          </Link>
+        </li>
+        <li>
+          <Link
+            className="flex items-center hover:text-neutral-800 dark:hover:text-neutral-100 transition-all"
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://github.com/vercel/next.js"
+          >
+            <ArrowIcon />
+            <p className="ml-2 h-7">github</p>
+          </Link>
+        </li>
+        <li>
+          <Link
+            className="flex items-center hover:text-neutral-800 dark:hover:text-neutral-100 transition-all"
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://vercel.com/templates/next.js/portfolio-starter-kit"
+          >
+            <ArrowIcon />
+            <p className="ml-2 h-7">view source</p>
+          </Link>
+        </li>
+      </ul>
+      <p className="mt-8 text-neutral-600 dark:text-neutral-300">
+        © {new Date().getFullYear()} MIT Licensed |{" "}
+        <Link
+          href="https://mcx.rocks"
+          className="hover:text-neutral-800 dark:hover:text-neutral-100 underline transition-all"
+        >
+          MCX
+        </Link>
+      </p>
+    </footer>
+  );
+}
---
+++ app/components/nav.tsx
@@ -0,0 +1,40 @@
+import Link from "next/link";
+
+const navItems = {
+  "/": {
+    name: "Home",
+  },
+  "/documents": {
+    name: "Documents",
+  },
+  "https://vercel.com/templates/next.js/portfolio-starter-kit": {
+    name: "Open source",
+  },
+};
+
+export function Navbar() {
+  return (
+    <aside className="mb-16 -ml-[8px] tracking-tight">
+      <div className="lg:top-20 lg:sticky">
+        <nav
+          className="relative md:relative flex flex-row items-start px-0 pb-0 md:overflow-auto scroll-pr-6 fade"
+          id="nav"
+        >
+          <div className="flex flex-row space-x-0 pr-10">
+            {Object.entries(navItems).map(([path, { name }]) => {
+              return (
+                <Link
+                  key={path}
+                  href={path}
+                  className="relative flex m-1 px-2 py-1 hover:text-neutral-800 dark:hover:text-neutral-200 align-middle transition-all"
+                >
+                  {name}
+                </Link>
+              );
+            })}
+          </div>
+        </nav>
+      </div>
+    </aside>
+  );
+}
